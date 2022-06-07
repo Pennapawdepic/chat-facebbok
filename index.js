bb54f65e90fc6790366cbb4481fb83ab8a2a5b34
Pennapawdepic <103092995+Pennapawdepic@users.noreply.github.com>
2022-06-07 16:02:52 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const VERIFY_TOKEN = "chatbot001";
 const { get } = require("lodash");
 const request = require("request");
 
-app.set("port", process.env.PORT || 3000);
+app.set("port", process.env.PORT || 4000);
 
 // Allows us to process the data
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -46,3 +46,35 @@ app.post("/webhook", async (req, res) => {
 app.listen(app.get("port"), function () {
   console.log("🚀 Server ready ~~~~");
 });
+
+const PAGE_ACCESS_TOKEN =
+  "EAADvAyOmQCcBAKrjrL4zI2C6tEwsuiEZAkuCGm9q4g7FBFPvr25BrT0cKSij70WWsHtZBmlc0pERt81VzbrrF2qesZCWVZAA0FgN9Mj9mZBqgi6XYzdFBNsKFa3FIEx9lWCE4vygzUYGPsNXmjopw3GgoDv5zSFr13itKsJqA68OZBCTEb2RgL";
+
+const handleEvents = (events) => {
+  const text = get(events, ["messaging", 0, "message", "text"]);
+  const sender = get(events, ["messaging", 0, "sender", "id"]);
+  const requestBody = {
+    messaging_type: "RESPONSE",
+    recipient: {
+      id: sender,
+    },
+    message: { text },
+  };
+
+  const config = {
+    method: "post",
+    uri: "https://graph.facebook.com/v12.0/me/messages",
+    json: requestBody,
+    qs: {
+      access_token: `${PAGE_ACCESS_TOKEN}`,
+    },
+  };
+  return request(config, (err, res, body) => {
+    if (!body.error) {
+      console.log("message sent!", body);
+      return body;
+    } else {
+      return new Error("Unable to send message:" + body.error);
+    }
+  });
+};
