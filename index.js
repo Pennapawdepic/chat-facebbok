4dee9ddff51bd815a6845a3c472902c510c6269b
Pennapawdepic <103092995+Pennapawdepic@users.noreply.github.com>
2022-06-07 14:52:53 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,56 +1,46 @@
-"use strict";
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const VERIFY_TOKEN = "chatbot001";
 
-// Imports dependencies and set up http server
-const express = require("express"),
-  bodyParser = require("body-parser"),
-  app = express().use(bodyParser.json()); // creates express http server
+app.set("port", process.env.PORT || 3000);
 
-// Sets server port and logs message on success
-app.listen(process.env.PORT || 8080, () => console.log("webhook is listening"));
+// Allows us to process the data
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-// Creates the endpoint for our webhook
-app.post("/webhook", (req, res) => {
-  let body = req.body;
+// ROUTES
+app.get("/", function (req, res) {
+  res.send("Hello ^____^");
+});
 
-  // Checks this is an event from a page subscription
+// Facebook GET
+app.get("/webhook", async (req, res) => {
+  // Parse the query params
+  const mode = req.query["hub.mode"];
+  const token = req.query["hub.verify_token"];
+  const challenge = req.query["hub.challenge"];
+  if (mode === "subscribe" && token === VERIFY_TOKEN) {
+    res.send(challenge);
+  } else {
+    // Responds with '403 Forbidden' if verify tokens do not match
+    console.log("WEBHOOK_VERIFIED");
+    res.sendStatus(403);
+  }
+});
+
+app.post("/webhook", async (req, res) => {
+  const { body } = req;
   if (body.object === "page") {
-    // Iterates over each entry - there may be multiple if batched
-    body.entry.forEach(function (entry) {
-      // Gets the message. entry.messaging is an array, but
-      // will only ever contain one message, so we get index 0
-      let webhook_event = entry.messaging[0];
-      console.log(webhook_event);
-    });
-
-    // Returns a '200 OK' response to all requests
-    res.status(200).send("EVENT_RECEIVED");
+    const events = body && body.entry && body.entry[0];
+    await handleEvents(events);
   } else {
     // Returns a '404 Not Found' if event is not from a page subscription
     res.sendStatus(404);
   }
+  return res.sendStatus(200);
 });
 
-// Adds support for GET requests to our webhook
-app.get("/webhook", (req, res) => {
-  // Your verify token. Should be a random string.
-  let VERIFY_TOKEN =
-    "EAADvAyOmQCcBABN2YzU8O9DfN3n6UBcWQLzKnIqttwwZAummbWDsXnrHKsij0JRl2RfjxxcXaYpLyiUsTPkfdPg7ZBGhmvsE4ZBcmfu2slbkwpUSYRF42oJxhlW1SuKtBPG0QUjqin0jZCdl2TQqdvP62bqxYjY1XsD58yMThSqRycBHfq9H";
-
-  // Parse the query params
-  let mode = req.query["hub.mode"];
-  let token = req.query["hub.verify_token"];
-  let challenge = req.query["hub.challenge"];
-
-  // Checks if a token and mode is in the query string of the request
-  if (mode && token) {
-    // Checks the mode and token sent is correct
-    if (mode === "subscribe" && token === VERIFY_TOKEN) {
-      // Responds with the challenge token from the request
-      console.log("WEBHOOK_VERIFIED");
-      res.status(200).send(challenge);
-    } else {
-      // Responds with '403 Forbidden' if verify tokens do not match
-      res.sendStatus(403);
-    }
-  }
+app.listen(app.get("port"), function () {
+  console.log("🚀 Server ready ~~~~");
 });
